b907ae283a372764c230040b0153cc8a7f819d98
Ritwik <sritwik03@gmail.com>
2023-02-02 18:39:39 +0530

    initial commit

---
+++ server/middleware/user.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const user = (req, res, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided.' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Invalid token.' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+module.exports = user;
